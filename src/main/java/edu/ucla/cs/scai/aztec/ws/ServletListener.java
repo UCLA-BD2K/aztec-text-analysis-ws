11082f7c524c2c572b96ee24b00a9d54ce8ba2ca
Massimo <mazzeo@ucla.edu>
2016-05-03 18:58:16 -0700

    Improved caching mechanism and similarity computed with tfidf and keywords separately

--- src/main/java/edu/ucla/cs/scai/aztec/ws/ServletListener.java
+++ src/main/java/edu/ucla/cs/scai/aztec/ws/ServletListener.java
@@ -22,6 +22,7 @@ public class ServletListener implements ServletContextListener {
         System.out.println("Entries path system property: "+System.getProperty("entries.path"));
         System.out.println("Publications path system property: "+System.getProperty("publications.path"));
         System.out.println("Representatives words path system property: "+System.getProperty("representativeWords.path"));
+        System.out.println("Publications path system property: "+System.getProperty("keywords.path"));
         Timer timerIndice = new Timer();
         TimerTask taskUpdate = new UpdateDocuments();
         timerIndice.schedule(taskUpdate, 30000l, 60000l * 30); //starts after 30 seconds from deployment is repeater every 30 minutes
